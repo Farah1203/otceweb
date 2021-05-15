67940ed7ad4492aef234dc83e561e82f22949ce2
farah <farah.toumi01@gmail.com>
2021-05-15 22:32:00 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HelpComponent } from './help/help.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SplashscreenComponent } from './splashscreen/splashscreen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path:'',
+    component: HomeComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'help',
+    component:HelpComponent
+  },
+  {
+    path :'profile',
+    component:ProfileComponent
+  },
+  {
+    path:'splashscreen',
+    component:SplashscreenComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  /**
+* Template Name: Selecao - v2.3.1
+* Template URL: https://bootstrapmade.com/selecao-bootstrap-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+
+
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+}
